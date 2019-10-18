6d30781a065065ee41f2f34991c830a64ee81e08
Murodjon <tmurodon@gmail.com>
2019-10-18 15:39:05 +0100

    Updates

---
+++ precache-manifest.9fbd30403787dede612e3f1de9a0443c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1668123c34db6b1734ded4bace54b0f",
+    "url": "/stopwatch/index.html"
+  },
+  {
+    "revision": "927801b59a2b96de9dd8",
+    "url": "/stopwatch/static/css/main.378bdc05.chunk.css"
+  },
+  {
+    "revision": "70a89e29d20dbdc318ef",
+    "url": "/stopwatch/static/js/2.282802b6.chunk.js"
+  },
+  {
+    "revision": "927801b59a2b96de9dd8",
+    "url": "/stopwatch/static/js/main.37420654.chunk.js"
+  },
+  {
+    "revision": "1c369f68847876b7b8e7",
+    "url": "/stopwatch/static/js/runtime-main.2af4395e.js"
+  }
+]);
